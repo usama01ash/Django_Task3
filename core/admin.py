f1655a32b53a53abfa09bcdf3b16d61e89e8dd14
usama01ash <usama.ashfaq@arbisoft.com>
2022-07-06 16:34:49 +0500

    Added Dummy Dates creation
    Added part of 100 dummy date creation in create_dummydata management
    command and registered DateTimeData model with admin to see it On
    admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from core.models import DateTimeData
+
 # Register your models here.
+
+
+admin.site.register(DateTimeData)
--- core/management/commands/create_dummydata.py
+++ core/management/commands/create_dummydata.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 
+from core.models import DateTimeData
+
 
 class Command(BaseCommand):
     help = 'Creates 100 dummy users and datetime enteries'
@@ -15,3 +17,11 @@ class Command(BaseCommand):
                 length=5), password=get_random_string(length=8))
 
         self.stdout.write(self.style.SUCCESS('100 users added to database'))
+
+        self.stdout.write(self.style.HTTP_INFO(
+            'Adding 100 dummy PST datetime objects in database'))
+        for i in range(100):
+            date_time = DateTimeData()
+            date_time.save()
+
+        self.stdout.write(self.style.SUCCESS('100 PST datetime added'))
