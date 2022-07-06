2132fbf428b77fdbb9f93aa6c21b391ffd558c4d
usama01ash <usama.ashfaq@arbisoft.com>
2022-07-06 16:09:37 +0500

    Added Model to save datetime values
    Model has naive datetime field and a Tz field to store its timezone.
    A pre_save signal is used for setting the PST time as date_time and
    Tz attribute automatically

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+
+import pytz
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class DateTimeData(models.Model):
+
+    date_time = models.DateTimeField()
+    Tz = models.CharField(max_length=3)
+
+    def __str__(self):
+        return str(self.date_time)
+
+
+@receiver(pre_save, sender=DateTimeData)
+def set_time_date(sender, instance, **kwargs):
+    if instance.id is None:
+        UTC_datetime = pytz.utc.localize(datetime.utcnow())  # Get UTC datetime
+        PST_tz = pytz.timezone('US/Pacific')
+        PST_datetime = UTC_datetime.astimezone(
+            PST_tz)  # Covert to PST datetime
+        instance.date_time = PST_datetime.replace(
+            tzinfo=None)  # Make it naive datetime for DB
+        instance.Tz = 'PST'
