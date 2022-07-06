1496361aa93d586aecdade8f2270ced1b0637874
usama01ash <usama.ashfaq@arbisoft.com>
2022-07-06 11:16:11 +0500

    Added configuration of PostgreSQL integration and core app

--- DjangoTask3/settings.py
+++ DjangoTask3/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'django_crontab',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'DjangoTask3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'managementcommand',
+        'USER': 'mainuser',
+        'PASSWORD': 'admin12',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
