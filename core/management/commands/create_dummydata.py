62b3737e945c6eca2126e283fc7388a47f1214ee
usama01ash <usama.ashfaq@arbisoft.com>
2022-07-06 11:57:06 +0500

    Added stdout messages for dummy user creation initiation and success

--- core/management/commands/create_dummydata.py
+++ core/management/commands/create_dummydata.py
@@ -7,6 +7,11 @@ class Command(BaseCommand):
     help = 'Creates 100 dummy users and datetime enteries'
 
     def handle(self, *args, **kwargs):
+
+        self.stdout.write(self.style.HTTP_INFO(
+            'Creating 100 dummy users with random usernames and passwords'))
         for i in range(100):
             User.objects.create_user(username=get_random_string(
                 length=5), password=get_random_string(length=8))
+
+        self.stdout.write(self.style.SUCCESS('100 users added to database'))
