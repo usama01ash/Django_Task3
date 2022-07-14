a537bc9812d3a43adcb97249ffe0900c3ebce9df
usama01ash <usama.ashfaq@arbisoft.com>
2022-07-14 15:18:22 +0500

    Added Logger Config
    Logger is required to get logs from CronJob to debug while coding

--- DjangoTask3/settings.py
+++ DjangoTask3/settings.py
@@ -127,3 +127,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logger Config to be used by CronJob to check and debug during Cronjob production
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'CronLogs.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
